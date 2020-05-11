b083df5221514d95e94478fdb9f35319ed08567f
Raza <rtshareef@gmail.com>
2020-05-11 08:16:31 -0400

    chrome working

--- PhotoFilter/ViewController.swift
+++ PhotoFilter/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-
+    let context = CIContext()
+    var original: UIImage?
+    
     @IBOutlet var imageView: UIImageView!
     @IBAction func choosePhoto(){
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
@@ -20,9 +22,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
             navigationController?.present(picker,animated: true,completion: nil)
         }
     }
+    
+    @IBAction func applySepia(){
+        guard let original = original else {
+            return
+        }
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        filter?.setValue(CIImage(image:original), forKey:kCIInputImageKey )
+        let output = filter?.outputImage
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+    }
+    
+    @IBAction func applyChrome(){
+        guard let original = original else {
+            return
+        }
+        
+        let filter = CIFilter(name: "CIColorMonochrome")
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        filter?.setValue(CIImage(image:original), forKey:kCIInputImageKey )
+        filter?.setValue(CIColor(red: 0, green: 7.8, blue: 9.7), forKey: kCIInputColorKey)
+        let output = filter?.outputImage
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        navigationController?.dismiss(animated: true, completion: nil)
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = image
+            original = image
         }
     }
 
