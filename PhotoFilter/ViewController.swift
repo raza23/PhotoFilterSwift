c5a255d2b32d757038432c7bcd814f8f6765e21c
Raza <rtshareef@gmail.com>
2020-05-11 07:18:32 -0400

    photo choosable

--- PhotoFilter/ViewController.swift
+++ PhotoFilter/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func choosePhoto(){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+//            Built in swift method that gives permission to use User's photos
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            navigationController?.present(picker,animated: true,completion: nil)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+        }
     }
 
 
